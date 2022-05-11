1b35a611d33f027db1f4dfd18fb60ef082e72bd4
dibyamgr <dibyamagar5@gmail.com>
2022-05-11 21:29:48 +0545

    Auth API Register

--- routes/auth.js
+++ routes/auth.js
@@ -3,14 +3,17 @@ const User = require("../models/User");
 
 // REGISTER
 router.post("/register", async (req, res) => {
-    const user = await new User({
-        username: "dibya",
-        email: "dibya@gmail.com",
-        password: "123456"
-    })
-
-    await user.save()
-    res.send("ok")
+    const newUser = new User({
+        username: req.body.username,
+        email: req.body.email,
+        password: req.body.password
+    });
+    try{
+        const user = await newUser.save();
+        res.status(200).json(user);
+    }catch(err){
+        console.log(err);
+    }
 });
 
 module.exports = router;

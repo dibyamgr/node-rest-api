3ed849016770ce39573ded8c79f8dcf201a69ab6
dibyamgr <dibyamagar5@gmail.com>
2022-05-11 21:42:46 +0545

    Auth API-LOGIN

--- routes/auth.js
+++ routes/auth.js
@@ -24,4 +24,22 @@ router.post("/register", async (req, res) => {
     }
 });
 
+// LOGIN
+router.post("/login", async (req, res) => {
+    try {
+        // findOne => its only one document inside it with same name
+        const user = await User.findOne({ email: req.body.email });
+        !user && res.status(404).send("user not found")
+
+        // checking password
+        const validPassword = await bcrypt.compare(req.body.password, user.password);
+        !validPassword && res.status(400).send("Password Not Matched");
+
+        res.status(200).json(user);
+    } catch (err) {
+        console.log(err)
+    }
+
+})
+
 module.exports = router;

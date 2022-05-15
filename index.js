ecc156ff43d8460ecc8faac3513c5202e170a4f6
dibyamgr <dibyamagar5@gmail.com>
2022-05-15 20:59:59 +0545

    Create a Post API

--- index.js
+++ index.js
@@ -6,6 +6,7 @@ const helmet = require("helmet");
 const morgan = require("morgan");
 const userRoute = require("./routes/users")
 const authRoute = require("./routes/auth")
+const postRoute = require("./routes/posts")
 
 // to use dot env
 dotenv.config();
@@ -24,6 +25,7 @@ app.use(morgan("common"))
 // Routes
 app.use("/api/users", userRoute)
 app.use("/api/auth", authRoute)
+app.use("/api/posts", postRoute)
 
 // Port
 app.listen(8800, () => {

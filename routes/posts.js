86b69cdf90b4e9c194fc3f362bded4acda3da556
dibyamgr <dibyamagar5@gmail.com>
2022-05-15 21:09:28 +0545

    Delete Posts API

--- routes/posts.js
+++ routes/posts.js
@@ -30,6 +30,20 @@ router.put("/:id", async (req, res) => {
     }
 })
 //delete a post
+router.delete("/:id", async (req, res) => {
+    try {
+        const post = await Post.findById(req.params.id);
+        if (post.userId === req.body.userId) {
+            await post.deleteOne();
+            res.status(200).json("The posts have been deleted!!")
+        } else {
+            res.status(403).json("You can delete only your post")
+        }
+
+    } catch (err) {
+        res.status(500).json(err)
+    }
+})
 //like a post
 //get a post
 //get timeline posts
